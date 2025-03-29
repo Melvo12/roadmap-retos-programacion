79045bb0d54cd0e199a7c5e9e516a20f09d6d612
Daniel <danielfrancisco12@gmail.com>
2025-03-28 22:13:59 -0500

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Melvo12.js
@@ -0,0 +1,49 @@
+// Sitio Oficial Javascript: https://www.javascript.com/
+
+/* Esta es otra manera 
+de colocar 
+comentarios en más de una línea */
+
+// Crea una variable (y una constante si el lenguaje lo soporta).
+
+
+var variable1 
+const constante2 = 10
+let varible3
+variable
+ /*
+ *   Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ */
+
+
+
+// String (Cadena de texto)
+let cadena = "Hola, mundo";
+
+// Number (Número: Entero o decimal)
+let entero = 42;
+let decimal = 3.14;
+
+// Boolean (Booleano: true o false)
+let verdadero = true;
+let falso = false;
+
+// Undefined (Indefinido: una variable declarada pero sin valor asignado)
+let indefinido;
+
+// Null (Nulo: ausencia intencionada de valor)
+let nulo = null;
+
+// Symbol (Símbolo: identificadores únicos)
+let simbolo = Symbol("descripcion");
+
+// BigInt (Números enteros grandes)
+let numeroGrande = 9007199254740991n;
+/*
+ * Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ */
+
+const holaMundo = '¡Hola, Javascript! '
+
+console.log(holaMundo);
